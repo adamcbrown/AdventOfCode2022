5b1f9dbd5154ad62426b8e476aabe25399ba2f7e
Adam Brown <adamcbrown1997@gmail.com>
2022-12-02 00:11:51 -0800

    Cleanedup unused code

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,71 +1,83 @@
-use pico_args::Error;
-
-enum Move {
-    Rock, Paper, Scissors
-}
-
 fn compute_score_1(opp: char, mine: char) -> u32 {
     match mine {
-        'X' => 1 + match opp {
-            'A' => 3,
-            'B' => 0,
-            'C' => 6,
-            _ => unreachable!(),
-        },
-        'Y' => 2 + match opp {
-            'A' => 6,
-            'B' => 3,
-            'C' => 0,
-            _ => unreachable!(),
-        },
-        'Z' => 3 + match opp {
-            'A' => 0,
-            'B' =>  6,
-            'C' =>  3,
-            _ => unreachable!(),
-        },
+        'X' => {
+            1 + match opp {
+                'A' => 3,
+                'B' => 0,
+                'C' => 6,
+                _ => unreachable!(),
+            }
+        }
+        'Y' => {
+            2 + match opp {
+                'A' => 6,
+                'B' => 3,
+                'C' => 0,
+                _ => unreachable!(),
+            }
+        }
+        'Z' => {
+            3 + match opp {
+                'A' => 0,
+                'B' => 6,
+                'C' => 3,
+                _ => unreachable!(),
+            }
+        }
         _ => unreachable!(),
     }
 }
 
 fn compute_score_2(opp: char, result: char) -> u32 {
     match result {
-        'X' => 0 + match opp {
-            'A' => 3,
-            'B' => 1,
-            'C' => 2,
-            _ => unreachable!(),
-        },
-        'Y' => 3 + match opp {
-            'A' => 1,
-            'B' => 2,
-            'C' => 3,
-            _ => unreachable!(),
-        },
-        'Z' => 6 + match opp {
-            'A' => 2,
-            'B' => 3,
-            'C' => 1,
-            _ => unreachable!(),
-        },
+        'X' => {
+            0 + match opp {
+                'A' => 3,
+                'B' => 1,
+                'C' => 2,
+                _ => unreachable!(),
+            }
+        }
+        'Y' => {
+            3 + match opp {
+                'A' => 1,
+                'B' => 2,
+                'C' => 3,
+                _ => unreachable!(),
+            }
+        }
+        'Z' => {
+            6 + match opp {
+                'A' => 2,
+                'B' => 3,
+                'C' => 1,
+                _ => unreachable!(),
+            }
+        }
         _ => unreachable!(),
     }
 }
 
-
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(input
-        .split("\n")
-        .map(|round| compute_score_1(round.chars().nth(0).unwrap(), round.chars().nth(2).unwrap()))
-        .fold(0, |a, b| a+b))
-    
+    Some(
+        input
+            .split("\n")
+            .map(|round| {
+                compute_score_1(round.chars().nth(0).unwrap(), round.chars().nth(2).unwrap())
+            })
+            .fold(0, |a, b| a + b),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    Some(input
-        .split("\n")
-        .map(|round| compute_score_2(round.chars().nth(0).unwrap(), round.chars().nth(2).unwrap()))
-        .fold(0, |a, b| a+b))
+    Some(
+        input
+            .split("\n")
+            .map(|round| {
+                compute_score_2(round.chars().nth(0).unwrap(), round.chars().nth(2).unwrap())
+            })
+            .fold(0, |a, b| a + b),
+    )
 }
 
 fn main() {
